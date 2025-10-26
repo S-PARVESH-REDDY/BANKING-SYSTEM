16c6f6288f217401d35679bf116c480d157fe8f3
unknown <parveshreddy8@gmail.com>
2025-10-27 01:41:53 +0530

    initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+public public class Main {
+    public static void main(String[] args) {
+        try {
+            javax.swing.UIManager.setLookAndFeel(javax.swing.UIManager.getCrossPlatformLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            new Login();
+        });
+    }
+} {
+    
+}
